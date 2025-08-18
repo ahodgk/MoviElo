afff10e8969bfc9f7724ee24590334a91dab21cc
Adam Hodgkinson <adam@adamhodgkinson.dev>
2025-08-18 18:35:35 +0100

    Update logger.ts

--- apps/web/app/lib/logger.ts
+++ apps/web/app/lib/logger.ts
@@ -1,15 +1,16 @@
 import pino from "pino";
 
-export const logger = pino({
-  level: "trace",
-  transport: {
-    target: "pino-pretty",
+const transport = import.meta.env.NODE_ENV === "development" ? {
+    target:  "pino-pretty",
     options: {
       colorize: true,
       translateTime: "SYS:standard",
       ignore: "pid,hostname",
     },
-  },
+  } : undefined
+export const logger = pino({
+  level: "trace",
+  transport,
 });
 
 logger.info("Logger initialized");
