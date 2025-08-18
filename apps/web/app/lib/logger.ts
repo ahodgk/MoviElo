4b03128664e8c1cc3f6cd8ed52a18185cb6ee505
Adam Hodgkinson <adam@adamhodgkinson.dev>
2025-08-18 19:11:25 +0100

    Update logger.ts

--- apps/web/app/lib/logger.ts
+++ apps/web/app/lib/logger.ts
@@ -8,9 +8,17 @@ const transport = import.meta.env.NODE_ENV === "development" ? {
       ignore: "pid,hostname",
     },
   } : undefined
+
+const formatters = {
+  level (label, number) {
+    return { level: label }
+  }
+}
+
 export const logger = pino({
   level: "trace",
   transport,
+  formatters,
 });
 
 logger.info("Logger initialized");
